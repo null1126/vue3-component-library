c5ac7c58e3e573c4fdc65ed9993c96e5d776aef5
xiaozhang8399 <1538583867@qq.com>
2025-03-15 18:04:00 +0800

    chore: 新增自动化发布包配置

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -1,13 +1,17 @@
 import type { App } from 'vue'
 import * as components from './components'
 
+export type { ButtonProps } from './button'
 export * from './components'
 
-export default {
-  install: (app: App) => {
-    for (const key in components) {
-      // @ts-expect-error: dynamic access
-      app.use(components[key])
-    }
+function install(app: App) {
+  for (const key in components) {
+    // @ts-expect-error: dynamic access
+    app.use(components[key])
   }
 }
+
+export default { install }
+
+export type { RadioProps } from './radio'
+export * from './types/components'
